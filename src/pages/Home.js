d4fd1497504143c7dd6bf9fed4821cb236802322
rod <rodrip0129@gmail.com>
2024-07-30 16:28:34 -0300

    add provisory page to teams

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div className="container mx-auto p-4 flex flex-col border rounded-xl shadow-lg bg-[rgba(255,255,255,0.75)] w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[50%] h-auto px-6 py-6 sm:px-8 sm:py-8 md:px-12 md:py-12 hover">
+    <div className="container mx-auto p-4 flex flex-col border rounded-xl shadow-lg bg-[rgba(255,255,255,0.75)] w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[50%] h-auto px-6 py-6 sm:px-8 sm:py-8 md:px-12 md:py-12">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4">Bienvenido a ATC Dream Match</h1>
       <p className="text-sm sm:text-base md:text-lg mb-4 font-semibold">
         ¡Bienvenido a ATC Dream Match! Aquí puedes crear y gestionar dos equipos
