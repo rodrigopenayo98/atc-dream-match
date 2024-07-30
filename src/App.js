1c4995876e2df134b5213e8da66e0d27cb8c85d4
rod <rodrip0129@gmail.com>
2024-07-30 12:23:42 -0300

    update App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Teams from './pages/Teams';
+import TeamDetail from './components/TeamDetail';
+import PlayerList from './components/PlayerList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/teams" component={Teams} />
+          <Route path="/team/:id" component={TeamDetail} />
+          <Route path="/players" component={PlayerList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
